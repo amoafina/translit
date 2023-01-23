d5138930ee4cc52478a8481c7e1ea7a40e026f64
wegetpage <kirill_fad@wegetpage.ru>
2023-01-23 14:05:09 +0300

    fixies

--- src/Parser.php
+++ src/Parser.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Translit;
+namespace Kogarkov\Translit;
 
 class Parser
 {
--- tests/ParserTest.php
+++ tests/ParserTest.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Translit;
+namespace Kogarkov\Translit;
 
 use PHPUnit\Framework\TestCase;
 
