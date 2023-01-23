f2a0e374253908e7d9506013105a7f499c3b3d4c
wegetpage <kirill_fad@wegetpage.ru>
2023-01-23 13:04:28 +0300

    init

---
+++ tests/ParserTest.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Translit;
+
+use PHPUnit\Framework\TestCase;
+
+class ParserTest extends TestCase
+{
+    public function testParseCorrectly(): void
+    {
+        $parser = new Parser();
+        $this->assertEquals(
+            'Privet, ya borsch',
+            $parser->go('Привет, я борщ')
+        );
+    }
+}
